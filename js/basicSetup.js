565e4a4e410c8665266dba42d98d6c0cbaea834b
william-shelly <william-shelly@users.noreply.github.com>
2021-02-07 09:32:10 -0600

    Adding a new test

--- js/basicSetup.js
+++ js/basicSetup.js
@@ -1,7 +1,12 @@
 export class BasicSetup {
 
+    constructor(container, message) {
+        this.container = container;
+        this.message = message;
+    }
+
     showTest(container, message) {
-        console.log(message);
+        // console.log(message);
         container.innerHTML = message;
     };
 }
--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 import { BasicSetup } from "./basicSetup.js"
 
-let basicSetup , basicSetupContainer, basicMessage;
-
-basicSetup = new BasicSetup();
+let setup1 , basicSetupContainer, basicMessage, headlineH1Setup, headlineContainerH1, headlineH1;
 
 basicSetupContainer = document.querySelector('#basic-container');
-basicMessage = 'basicSetup work!';
+basicMessage = 'Hello World!';
+
+headlineContainerH1 = document.querySelector('#headline-h1');
+headlineH1 = 'Hello JavaScript!';
+
+setup1 = new BasicSetup(basicSetupContainer, basicMessage);
+
+headlineH1Setup = new BasicSetup(headlineContainerH1, headlineH1);
 
 document.addEventListener('DOMContentLoaded', function(){
-    basicSetup.showTest(basicSetupContainer, basicMessage);
+    setup1.showTest(basicSetupContainer, basicMessage);
+    headlineH1Setup.showTest(headlineContainerH1, headlineH1);
 });
